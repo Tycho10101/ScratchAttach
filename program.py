2317e445838f09cc475d4d9ad3a7820fc815b898
Tycho10101 <107083420+Tycho10101@users.noreply.github.com>
2024-04-21 16:40:48 -1000

    Create program.py

---
+++ program.py
@@ -0,0 +1,12 @@
+import scratchattach as scratch3
+
+# sets up secret values
+username = os.getenv("username")
+password = os.getenv("password")
+projectID = os.getenv("projectID")
+
+# connects, sends, and disconects
+session = scratch3.login(username, password)
+conn = session.connect_cloud(projectID)
+conn.set_var("1", "4212024")
+conn.disconnect()
