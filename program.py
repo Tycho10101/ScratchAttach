2ed26e1aab8b015d542b97f98349e010e52c88da
Tycho10101 <107083420+Tycho10101@users.noreply.github.com>
2024-04-21 16:49:23 -1000

    Update program.py

--- program.py
+++ program.py
@@ -4,8 +4,6 @@ from dotenv import load_dotenv
 import os
 
 # sets up secret values
-load_dotenv()
-
 username = os.getenv("username")
 password = os.getenv("password")
 projectID = os.getenv("projectID")
